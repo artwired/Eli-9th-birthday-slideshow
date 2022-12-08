339df0746c9f563f847e8ea6b413cce9424bf30d
Brad Cole <artwired@users.noreply.github.com>
2022-12-08 14:11:06 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ getData()
     // the main template adding JSON paths to data
     const petTemplate = anEvent => {
         return `
-        <div class="activity mySlides fade">
-            <img class="event-photo" style="width:100%" src="${anEvent.photo}">
+        <div class="mySlides">
+            <img class="event-photo" src="${anEvent.photo}">
             <h2 class="sub-heading">${anEvent.location}</h2>
             <h3 class="event-name">${anEvent.event}</h3>
             <p class="the-month">${anEvent.month}</p>
@@ -27,57 +27,34 @@ getData()
         </div>
         `
     }
-
 // Outputting the template with JSON data
     document.querySelector("#app").innerHTML = `
         <h1 class="title">Happy Birthday Eli!</h1>
-        <p class="intro-text">Here are some fun events you experienced in 2022</p>
-        <div class="activity-container slideshow-container">
+        <p class="">Here are some fun events you experienced in 2022</p>
+        <div class="slideshow-container">
         ${workingData.map(petTemplate).join('')}
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
         </div>
-        <div style="text-align:center">
-            <span class="dot" onclick="currentSlide(1)"></span>
-            <span class="dot" onclick="currentSlide(2)"></span>
-            <span class="dot" onclick="currentSlide(3)"></span>
+        <div class="buttons">
+            <button id="prev">&#10094;</button>
+        </div>
+        <div class="buttons">
+            <button id="next">&#10095;</button>
         </div>
     `
 
+   function findActiveSlide (slide) {
+    return slide.id === 2
+   }
 
+   const theActiveSlide = workingData.find(findActiveSlide)
 
-    let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+   if (theActiveSlide) {
+    const getContainerDiv = document.querySelector('.mySlides')
+    getContainerDiv.classList.add("current")
+   }
+    
+    
 })
-
-
-
 // Error handling for JSON data processing
 .catch(err => {
     console.log('rejected:', err);
