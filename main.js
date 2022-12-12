31a850ac4a963ec5f2535d2e57b147435ec00c79
Brad Cole <artwired@users.noreply.github.com>
2022-12-12 12:00:49 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ getData()
     const next = document.querySelector("#next")
     const prev = document.querySelector("#prev")
     const auto = true
-    const intervalTime = 5000
+    const intervalTime = 8000
     let slideInterval
 
     const nextSlide = () => {
